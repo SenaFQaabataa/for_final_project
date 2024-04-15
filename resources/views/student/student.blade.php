a9b01b605ccfb8a47b6bb13ea39dd63ee647d877
Yishak Zewdineh <yishakzewdineh@gmail.com>
2024-04-15 12:48:55 +0300

    layout structure done

--- resources/views/student/student.blade.php
+++
@@ -1,9 +0,0 @@
-<h1 class="text-center color-red text-body text-RED bg-blue">JIMMA UNIVERSITY COMMUNITY BASED EDUCATION(CBE)</h1>
-
-@extends('cbe.cbe')
-
-@section('content')
-
-
-
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Auth\Events\Login;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DepartmentController;
 
 
 /*
@@ -20,15 +22,37 @@ Route::get('/', function () {
     return view('home');
 });
 
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/** CBE Routes Start */
+// TO DO: implement => middleware(['auth', 'user-access:admin']) on all grouped routes
+Route::prefix('cbe')->group(function () {
+    Route::get('/home', function (){
+        return view('pages.cbe.home');
+    })->name('cbe.home');
+
+    Route::prefix('/department')->group(function (){
+        Route::get('/add', [DepartmentController::class, 'create'])->name('cbe.department.add');
+        Route::post('/add', [DepartmentController::class, 'store'])->name('cbe.department.store');
+        Route::get('/list', function (){
+            return view('pages.cbe.department.list');
+        })->name('cbe.department.list');
+        Route::get('/view/{department}', function (){
+            return view('pages.cbe.department.view');
+        })->name('cbe.department.view');
+        Route::get('/edit/{department}', [DepartmentController::class, 'edit'])->name('cbe.department.edit');
+        Route::post('/update/{department}', [DepartmentController::class, 'update'])->name('cbe.department.update');
+        Route::get('/delete/{department}', [DepartmentController::class, 'destroy'])->name('cbe.department.delete');
+    });
+});
+
 Route::get('/student', function (){
     return view('student.student');
 });
 
-Route::get('/cbe', function (){
-    return view('cbe.cbe');
-});
 
-Route::get('/login',[UserController::class, 'login'])->name('login')->middleware('guest');
+// Route::get('/login',[UserController::class, 'login'])->name('login')->middleware('guest');
 
 
-Route::post('users/authenticate',[UserController::class, 'authenticate']);
+// Route::post('users/authenticate',[UserController::class, 'authenticate']);
